5661984d6038a5eda2418209ad5644ef70a29b56
KlumpenM <ruben300799@gmail.com>
2024-12-05 18:37:09 +0100

    Working on part2, found upper and lower bound

--- day2/day2Ruben.py
+++ day2/day2Ruben.py
@@ -66,3 +66,56 @@ print(safe)
 Now we are allowed to remove 1 and only 1 level from the list.
 So we are making a list safe
 """
+safe = 0
+ascending = True
+amount_of_rules_removed = 0
+
+# Found out that it's between 479 and 557
+
+with open("day2/inputRuben.txt", "r") as f:
+    data = f.read().splitlines()
+    # Iterating through the data
+    for line in data:
+        # Splitting the line into a list of integers
+        levels = list(map(int, line.split()))
+        # Enumerating each integer in the list
+        for (i, _) in enumerate(levels):
+            # If the integer is the last one in the list, break the loop
+            if i == len(levels) - 1:
+                safe += 1
+                break
+            # If the integer is the same as the next
+            if levels[i] == levels[i + 1]:
+                if levels[i] == levels[i + 1]:
+                    break
+                amount_of_rules_removed += 1
+                continue
+
+            # If the integer is smaller than the next
+            if levels[i] < levels[i + 1]:
+                if i == 0: 
+                    ascending = True
+                if i == len(levels) - 3:
+                    if levels[i + 1] - levels[i] > 3 and levels[i + 2] - levels[i] > 3:
+                        break
+                # If the difference between the integer and the next is greater than 3, break the loop
+                if levels[i + 1] - levels[i] > 3 or not ascending:
+                    amount_of_rules_removed += 1
+                    continue
+                else:
+                    continue
+            
+            # If the integer is greater than the next
+            if levels[i] > levels[i + 1]:
+                if i == 0:
+                    ascending = False
+                if i == len(levels) - 3:
+                    if levels[i] - levels[i + 1] > 3 and levels[i] - levels[i + 2] > 3:
+                        break
+                # If the difference between the integer and the next is greater than 3, break the loop
+                if levels[i] - levels[i + 1] > 3 or ascending:
+                    amount_of_rules_removed += 1
+                    break
+                else:
+                    continue
+print(safe)
