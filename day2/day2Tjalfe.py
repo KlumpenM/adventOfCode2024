4106a1523d292d0ab74e81fe32ca6fefa344c76e
Tjalfe <ReptilNecrosisGaming@gmail.com>
2024-12-05 15:16:14 +0100

    I do not know what goes wrong here, I have debugged for an hour and every case goes through as it should, there must be some edge case that is not considered, unfinished.

--- day2/day2Tjalfe.py
+++ day2/day2Tjalfe.py
@@ -1,31 +1,55 @@
-
 safe = 0
-ascending = True
-
-with open("./day2/inputTjalfe.txt") as f:
+with open("./day2/inputTjalfeTest.txt") as f:
     file = f.read().splitlines()
 
     for line in file:
         levels = list(map(int, line.split()))
-        for i in range(len(levels)):
-            if (i == len(levels)-1):
+        ascending = True
+        unsafe = False
+        secondUnsafety = False
+        removedElements = 0
+        for i in range(len(line)):
+            if (i > 1 and removedElements > 0):
+                i = i-removedElements-1
+            else:
+                i = i-removedElements
+            if unsafe:
+                if secondUnsafety:
+                    break
+                unsafe = False
+                secondUnsafety = True
+            if (i == len(levels)-1 or i == len(levels)):
+                print(line)
                 safe += 1
                 break
             if (levels[i] == levels[i+1]):
-                break
+                unsafe = True
+                levels.pop(i)
+                removedElements += 1
+                continue
             if (levels[i] < levels[i+1]):
                 if (i == 0):
                     ascending = True
                 if ((levels[i+1] - levels[i]) > 3 or not ascending):
-                    break
-                else:
+                    unsafe = True
+                    if(not ascending):
+                        levels.pop(i)
+                        removedElements += 1
+                    else:
+                        levels.pop(i+1)
+                        removedElements += 1
                     continue
             if (levels[i] > levels[i+1]):
                 if (i == 0):
                     ascending = False
                 if ((levels[i] - levels[i+1]) > 3 or ascending):
-                    break
-                else:
+                    unsafe = True
+                    if(ascending):
+                        levels.pop(i)
+                        removedElements += 1
+                    else:
+                        levels.pop(i+1)
+                        removedElements += 1
                     continue
     print(safe)
             
