b3e04e6ef64a17951c2cfb72cc5030a8caf70431
KlumpenM <ruben300799@gmail.com>
2024-12-06 12:43:50 +0100

    Done with day 3 part 2

--- day3/day3Ruben.py
+++ day3/day3Ruben.py
@@ -9,3 +9,27 @@ with open('day3/inputRuben.txt') as f:
         ans += int(left) * int(right)
 
 print(ans)
+
+
+
+#################
+#### PART 2 #####
+#################
+ans = 0
+do = True
+
+with open('day3/inputRuben.txt') as f:
+    data = f.read().strip()
+    results = re.findall("mul\([0-9]+,[0-9]+\)|do\(\)|don\'t\(\)", data)
+    for mul in results:
+        if mul == "don't()":
+            do = False
+        elif mul == "do()":
+            do = True
+        if do and mul.startswith("mul"):
+            left, right = mul[4:-1].split(",")
+            ans += int(left) * int(right)
+
+print(ans)
+
+
